396fb8d9675670362278b72cb8d080663b7c1861
PreciousAimuan <precious.aimuan@decagon.dev>
2024-01-22 08:12:47 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace RemoveIndex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Here: ");
+            string userString = Console.ReadLine();
+            Console.WriteLine("Enter Index: ");
+            int Index = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(userString.Remove(Index, 1));
+        }
+    }
+}
